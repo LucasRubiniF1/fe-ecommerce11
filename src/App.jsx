72c56fa54ab163ae73638eac6989986d9b4b4606
lucasRubiniF1 <lrubini@uade.edu.ar>
2024-10-24 18:49:58 -0300

    ABM Products: Hooks: UseFetch con axios creado; Components: ProductForm, ErrorAlert Agregados; Pages ProductCreate; Product Details; ProductEdit; ProductSearchs agregads

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-//import React from 'react';
+import React from 'react';
 import './index.css';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Navbar from './components/Navbar';
@@ -6,28 +6,29 @@ import Sidebar from './components/Sidebar';
 import Footer from './components/Footer';
 import ProductSearch from './pages/ProductSearch';
 import ProductDetail from './pages/ProductDetail';
+import ProductEdit from './pages/ProductEdit';
+import ProductCreate from './pages/ProductCreate';
 
 const App = () => {
   return (
-    
-<>
-    <Navbar />    
-
-    <Router>
-
-      <div className="app-container">  
-        <div className="main-content">
-          <Sidebar />
-          <div className="page-content">
-            <Routes>
-              <Route path="/" element={<ProductSearch />} />
-              <Route path="/product/:id" element={<ProductDetail />} />
-            </Routes>
+    <>
+      <Navbar />
+      <Router>
+        <div className="app-container">
+          <div className="main-content">
+            <Sidebar />
+            <div className="page-content">
+              <Routes>
+                <Route path="/" element={<ProductSearch />} />
+                <Route path="/product/:id" element={<ProductDetail />} />
+                <Route path="/product/edit/:id" element={<ProductEdit />} />
+                <Route path="/product/create" element={<ProductCreate />} />
+              </Routes>
+            </div>
           </div>
+          <Footer />
         </div>
-        <Footer />
-      </div>
-    </Router> 
+      </Router>
     </>
   );
 };
