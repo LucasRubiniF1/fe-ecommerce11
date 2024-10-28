ab85614c5638ffb093c4894780bf264434acb569
lucasvegalv <lucasvega02@gmail.com>
2024-10-28 19:50:18 -0300

    fix: Conflicts solved

--- src/App.jsx
+++ src/App.jsx
@@ -5,31 +5,12 @@ import Navbar from './components/Navbar';
 import Footer from './components/Footer';
 import ProductSearch from './pages/ProductSearch';
 import ProductDetail from './pages/ProductDetail';
-<<<<<<< HEAD
 import Cart from './pages/Cart';
 import Checkout from './pages/Checkout';
-
-const App = () => {
-  return (
-    
-<>
-    <Navbar />    
-
-    <Router>
-
-      <div className="app-container">  
-        <div className="main-content">
-          {/* <Sidebar /> */}
-          <div className="page-content">
-            <Routes>
-              <Route path="/" element={<ProductSearch />} />
-              <Route path="/product/:id" element={<ProductDetail />} />
-              <Route path="/cart/:userId" element={ <Cart/> }/>
-              <Route path="/checkout/:userId" element={ <Checkout/> }/>
-            </Routes>
-=======
-import ProductEdit from './pages/ProductEdit';
+import Sidebar from './components/Sidebar';
 import ProductCreate from './pages/ProductCreate';
+import ProductEdit from './pages/ProductEdit';
+
 
 const App = () => {
   return (
@@ -45,9 +26,10 @@ const App = () => {
                 <Route path="/product/:id" element={<ProductDetail />} />
                 <Route path="/product/edit/:id" element={<ProductEdit />} />
                 <Route path="/product/create" element={<ProductCreate />} />
+                <Route path="/cart/:userId" element={ <Cart/> }/>
+              <Route path="/checkout/:userId" element={ <Checkout/> }/>
               </Routes>
             </div>
->>>>>>> main
           </div>
           <Footer />
         </div>
