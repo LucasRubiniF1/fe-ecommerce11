8adb91e43dac10134462b2116ba2377a9ff9eb42
Matias Abate <matiasalessandro1999@gmail.com>
2024-10-29 15:59:34 -0300

    Redireccion de FaUser a MyAccount

--- src/App.jsx
+++ src/App.jsx
@@ -1,37 +1,36 @@
-import React from 'react';
-import './index.css';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Navbar from './components/Navbar';
-import Sidebar from './components/Sidebar';
-import Footer from './components/Footer';
-import ProductSearch from './pages/ProductSearch';
-import ProductDetail from './pages/ProductDetail';
-import ProductEdit from './pages/ProductEdit';
-import ProductCreate from './pages/ProductCreate';
+import React from "react";
+import "./index.css";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Sidebar from "./components/Sidebar";
+import Footer from "./components/Footer";
+import ProductSearch from "./pages/ProductSearch";
+import ProductDetail from "./pages/ProductDetail";
+import ProductEdit from "./pages/ProductEdit";
+import ProductCreate from "./pages/ProductCreate";
 import Account from "./pages/Account";
 
 const App = () => {
   return (
-    <>
-      <Navbar />
-      <Router>
-        <div className="app-container">
-          <div className="main-content">
-            <Sidebar />
-            <div className="page-content">
-              <Routes>
-                <Route path="/" element={<ProductSearch />} />
-                <Route path="/product/:id" element={<ProductDetail />} />
-                <Route path="/product/edit/:id" element={<ProductEdit />} />
-                <Route path="/product/create" element={<ProductCreate />} />
-                <Route path="/account" element={<Account />} />
-              </Routes>
-            </div>
+    <Router>
+      <div className="app-container">
+        <Navbar />
+        <div className="main-content">
+          <Sidebar />
+          <div className="page-content">
+            <Routes>
+              <Route path="/" element={<ProductSearch />} />
+              <Route path="/product/:id" element={<ProductDetail />} />
+              <Route path="/product/edit/:id" element={<ProductEdit />} />
+              <Route path="/product/create" element={<ProductCreate />} />
+              <Route path="/account" element={<Account />} />
+            </Routes>
           </div>
-          <Footer />
         </div>
-      </Router>
-    </>
+        <Footer />
+      </div>
+    </Router>
   );
 };
+
 export default App;
