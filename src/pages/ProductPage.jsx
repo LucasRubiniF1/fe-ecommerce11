0ba07f735806719d62c66723843429fb0eb6082b
shir011 <108352884+shir011@users.noreply.github.com>
2024-11-03 20:01:23 -0300

    cambio

--- src/pages/ProductPage.jsx
+++ src/pages/ProductPage.jsx
@@ -7,7 +7,7 @@ const ProductPage = () => {
   const { category } = useParams(); // Captura el parámetro de categoría de la URL
 
   // Filtra los productos según la categoría obtenida del parámetro
-  const filteredProducts = productsData.filter((product) => product.category === category);
+  const filteredProducts = productsData.products.filter((product) => product.category === category);
 
   return (
     <div className="flex">
