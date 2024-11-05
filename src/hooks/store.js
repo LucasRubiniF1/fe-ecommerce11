ab2b5f8f2c93fa1734e726de9c00ea33e974c0ba
shir011 <108352884+shir011@users.noreply.github.com>
2024-11-05 19:33:30 -0300

    modificaciones

--- src/hooks/store.js
+++ src/hooks/store.js
@@ -8,15 +8,13 @@ const useStore = create(
       wishlist: [],
 
       addToCart: (product, userId) => set((state) => {
-        // Verifica si el producto tiene stock
+        
         if (product.stock <= 0) {
-            // Si no hay stock, no se añade al carrito
-            return state; // O puedes lanzar un error, dependiendo de tu manejo de errores
+            return state; 
         }
     
         const exists = state.cart.find(item => item.id === product.id);
         if (exists) {
-            // Si el producto ya existe en el carrito, aumenta la cantidad
             return {
                 cart: state.cart.map(item =>
                     item.id === product.id
@@ -25,20 +23,18 @@ const useStore = create(
                 ),
             };
         }
-        // Si el producto no existe en el carrito, lo añade con cantidad 1
         return { cart: [...state.cart, { ...product, quantity: 1 }] };
     }),
     
     updateQuantity: (itemId, newQuantity) => {
         set((state) => {
-            // Si la nueva cantidad es cero, eliminamos el producto del carrito
             if (newQuantity <= 0) {
                 return {
                     cart: state.cart.filter(item => item.id !== itemId),
                 };
             }
             
-            // De lo contrario, actualizamos la cantidad del producto
+           
             return {
                 cart: state.cart.map((item) =>
                     item.id === itemId ? { ...item, quantity: newQuantity } : item
