6621c0ea27d10adf7b6479d829e64acf954258a1
shir011 <108352884+shir011@users.noreply.github.com>
2024-11-06 00:05:52 -0300

    actual

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,12 +1,15 @@
 import { useState } from 'react';
-import { FaUser, FaShoppingCart, FaSearch, FaHeart } from "react-icons/fa";
+import { FaUser, FaShoppingCart, FaSearch, FaHeart, FaSignOutAlt } from "react-icons/fa";
 import { useNavigate } from "react-router-dom"; 
 import PanelCategories from './PanelCategories'
+import { useAuth } from '../context/AuthContext'; 
 
 const Navbar = ({ onSearch }) => {
   const [searchTerm, setSearchTerm] = useState('');
   const [showSearchBar, setShowSearchBar] = useState(false);
   const navigate = useNavigate();
+  const {user, logout } = useAuth();
+
 
   const toggleSearchBar = () => {
     setShowSearchBar(!showSearchBar);
@@ -23,6 +26,12 @@ const Navbar = ({ onSearch }) => {
   const handleUserClick = () => {
     navigate("/login");
   };
+
+  const handleLogoutClick = () => {
+    logout(); 
+    navigate("/"); 
+  };
+
 
   const goToHome = () => {
     navigate("/");
@@ -63,6 +72,17 @@ const Navbar = ({ onSearch }) => {
         <li className="text-black hover:text-gray-600 cursor-pointer" onClick={handleSearchClick}>
           <FaSearch size={20} />
         </li>
+        {user ? (
+          <div className="relative">
+            {/* Mostrar el nombre del usuario si esta logueado */}
+            <li className="font-semibold text-black hover:text-gray-600 cursor-pointer">Hola {user.name}!</li>
+            
+          </div>
+        ) : (
+          <li className="text-black hover:text-gray-600 cursor-pointer" onClick={() => navigate("/login")}>
+            <FaUser size={20} />
+          </li>
+        )}
         <div className="relative">
           <li className="text-black hover:text-rose-600 cursor-pointer" onClick={handleWishlistClick}>
             <FaHeart size={20} />
@@ -73,14 +93,25 @@ const Navbar = ({ onSearch }) => {
           <li className="text-black hover:text-gray-600 cursor-pointer" onClick={handleCartClick}>
             <FaShoppingCart size={20} />
           </li>
-        
         </div>
-        <li className="text-black hover:text-gray-600 cursor-pointer" onClick={handleUserClick}>
-          <FaUser size={20} />
-        </li>
+        <div className="relative">
+          <li className="text-black hover:text-gray-600 cursor-pointer" onClick={handleLogoutClick}>
+              <FaSignOutAlt size={20} />
+          </li>
+          </div>
+        
       </ul>
     </nav>
   );
 };
 
 export default Navbar;
+
+
+
+
+ 
+  
+
+
+  
