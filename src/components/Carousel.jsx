eb3f459e16fef55ff3ea60ffe179a21cf6636db0
Matias Abate <matiasalessandro1999@gmail.com>
2024-11-10 00:14:40 -0300

    Soluciones varias sobre el login, carrousel, token, account

--- src/components/Carousel.jsx
+++ src/components/Carousel.jsx
@@ -1,18 +1,18 @@
-import React, { useState } from 'react';
-import { useNavigate } from 'react-router-dom';
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
 import { FaShoppingCart, FaHeart } from "react-icons/fa";
 import useStore from "../hooks/UseStore.js";
-import { useAuth } from "../context/AuthContext.jsx"; 
+import { useAuth } from "../hooks/UseAuth.js";
 
 const Carousel = ({ products, titulo }) => {
   const [startIndex, setStartIndex] = useState(0);
   const itemsPerPage = 4;
   const navigate = useNavigate();
-  const { user, isAuthenticated } = useAuth(); 
-  const userId = user ? user.id : null;
+  const { user } = useAuth();
+  //const { user, isAuthenticated } = useAuth();
+  //const userId = user ? user.id : null;
   const { addToCart, wishlist, addToWishlist, removeFromWishlist } = useStore();
-  console.log(userId);
-
+  //console.log(userId);
 
   // Pagination controls
   const nextPage = () => {
@@ -33,10 +33,10 @@ const Carousel = ({ products, titulo }) => {
 
   // Toggle wishlist status
   const handleWishlistToggle = (product) => {
-    if (wishlist.some((item) => item.id === product.id)) {
+    if (wishlist?.some((item) => item.id === product.id)) {
       removeFromWishlist(product.id);
     } else {
-      addToWishlist(product);
+      addToWishlist(product, user.id);
     }
   };
 
@@ -53,60 +53,85 @@ const Carousel = ({ products, titulo }) => {
         </button>
 
         <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
-          {products.slice(startIndex, startIndex + itemsPerPage).map((product) => {
-            // Check if the product is in the wishlist
-            const isInWishlist = wishlist.some((item) => item.id === product.id);
-            
-            return (
-              <div
-                key={product.product_id}
-                className="group relative border-0 rounded-2xl p-6 shadow-[0_8px_30px_rgb(0,0,0,0.04)] transition-all duration-300 hover:shadow-[0_8px_30px_rgb(0,0,0,0.08)] hover:-translate-y-1 bg-white overflow-hidden"
-              >
-                {/* Badge de stock */}
-                <div className="absolute top-4 right-4 bg-gray-50 px-3 py-1 rounded-full">
-                  <p className="text-xs font-medium text-gray-600">Stock: {product.stock}</p>
-                </div>
+          {products
+            .slice(startIndex, startIndex + itemsPerPage)
+            .map((product) => {
+              // Check if the product is in the wishlist
+              const isInWishlist = wishlist?.some(
+                (item) => item.id === product.id
+              );
 
+              return (
+                <div
+                  key={product.product_id}
+                  className="group relative border-0 rounded-2xl p-6 shadow-[0_8px_30px_rgb(0,0,0,0.04)] transition-all duration-300 hover:shadow-[0_8px_30px_rgb(0,0,0,0.08)] hover:-translate-y-1 bg-white overflow-hidden"
+                >
+                  {/* Badge de stock */}
+                  <div className="absolute top-4 right-4 bg-gray-50 px-3 py-1 rounded-full">
+                    <p className="text-xs font-medium text-gray-600">
+                      Stock: {product.stock}
+                    </p>
+                  </div>
 
-                {/* Contenedor de imagen con efecto hover */}
-                <div className="relative mb-6 rounded-xl bg-gray-50 p-4 transition-transform duration-300 group-hover:scale-[1.02] cursor-pointer"
-                onClick={() => handleClick(product)}>
-                  <img
-                    src={product.images || 'https://http2.mlstatic.com/D_NQ_NP_2X_977897-MLU79321619721_092024-F.webp'}
-                    alt={product.name}
-                    className="w-full h-48 object-contain mix-blend-multiply"
-                  />
-                </div>
-
-                {/* Contenido de texto */}
-                <div className="space-y-2 cursor-pointer" 
-                onClick={() => handleClick(product)}>
-                  <h3 className="text-lg font-semibold text-gray-800 line-clamp-1">{product.name}</h3>
-                  <p className="text-sm text-gray-500 line-clamp-2 min-h-[2.5rem]">{product.description}</p>
-                  <p className="text-2xl font-bold text-gray-900">${product.price}</p>
-                </div>
-
-                {/* Botones con efecto hover mejorado */}
-                <div className="flex gap-3 mt-6">
-                  <button
-                    onClick={() => addToCart(product)}
-                    className="flex-1 bg-gray-900 text-white py-3 px-4 rounded-xl font-medium transition-all duration-300 hover:bg-gray-800 hover:shadow-lg flex items-center justify-center gap-2 text-sm"
+                  {/* Contenedor de imagen con efecto hover */}
+                  <div
+                    className="relative mb-6 rounded-xl bg-gray-50 p-4 transition-transform duration-300 group-hover:scale-[1.02] cursor-pointer"
+                    onClick={() => handleClick(product)}
                   >
-                    <FaShoppingCart size={11} />
-                    Add to Cart
-                  </button>
-                  <button
-                    onClick={() => handleWishlistToggle(product)}
-                    className={`p-3 rounded-xl border ${
-                      isInWishlist ? 'text-black border-black' : 'text-gray-600 border-gray-200'
-                    } hover:text-red-500 hover:border-red-500 transition-colors duration-300`}
+                    <img
+                      src={
+                        product.images ||
+                        "https://http2.mlstatic.com/D_NQ_NP_2X_977897-MLU79321619721_092024-F.webp"
+                      }
+                      alt={product.name}
+                      className="w-full h-48 object-contain mix-blend-multiply"
+                    />
+                  </div>
+
+                  {/* Contenido de texto */}
+                  <div
+                    className="space-y-2 cursor-pointer"
+                    onClick={() => handleClick(product)}
                   >
-                    <FaHeart size={20}  className={isInWishlist ? 'text-rose-600' : 'text-gray-600'}  />
-                  </button>
+                    <h3 className="text-lg font-semibold text-gray-800 line-clamp-1">
+                      {product.name}
+                    </h3>
+                    <p className="text-sm text-gray-500 line-clamp-2 min-h-[2.5rem]">
+                      {product.description}
+                    </p>
+                    <p className="text-2xl font-bold text-gray-900">
+                      ${product.price}
+                    </p>
+                  </div>
+
+                  {/* Botones con efecto hover mejorado */}
+                  <div className="flex gap-3 mt-6">
+                    <button
+                      onClick={() => addToCart(product)}
+                      className="flex-1 bg-gray-900 text-white py-3 px-4 rounded-xl font-medium transition-all duration-300 hover:bg-gray-800 hover:shadow-lg flex items-center justify-center gap-2 text-sm"
+                    >
+                      <FaShoppingCart size={11} />
+                      Add to Cart
+                    </button>
+                    <button
+                      onClick={() => handleWishlistToggle(product)}
+                      className={`p-3 rounded-xl border ${
+                        isInWishlist
+                          ? "text-black border-black"
+                          : "text-gray-600 border-gray-200"
+                      } hover:text-red-500 hover:border-red-500 transition-colors duration-300`}
+                    >
+                      <FaHeart
+                        size={20}
+                        className={
+                          isInWishlist ? "text-rose-600" : "text-gray-600"
+                        }
+                      />
+                    </button>
+                  </div>
                 </div>
-              </div>
-            );
-          })}
+              );
+            })}
         </div>
 
         <button
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,19 +1,19 @@
-import { useState } from 'react';
-import { FaUser, FaShoppingCart, FaSearch, FaHeart, FaSignOutAlt } from "react-icons/fa";
-import { useNavigate } from "react-router-dom"; 
-import PanelCategories from './PanelCategories'
-import { useAuth } from '../context/AuthContext'; 
+import { useState } from "react";
+import {
+  FaUser,
+  FaShoppingCart,
+  FaSearch,
+  FaHeart,
+  FaSignOutAlt,
+} from "react-icons/fa";
+import { useNavigate } from "react-router-dom";
+import PanelCategories from "./PanelCategories";
+import { useAuth } from "../hooks/UseAuth.js";
 
 const Navbar = ({ onSearch }) => {
-  const [searchTerm, setSearchTerm] = useState('');
-  const [showSearchBar, setShowSearchBar] = useState(false);
+  const [searchTerm, setSearchTerm] = useState("");
   const navigate = useNavigate();
-  const {user, logout } = useAuth();
-
-
-  const toggleSearchBar = () => {
-    setShowSearchBar(!showSearchBar);
-  };
+  const { user, logout } = useAuth();
 
   const handleSearchInput = (event) => {
     setSearchTerm(event.target.value);
@@ -24,14 +24,17 @@ const Navbar = ({ onSearch }) => {
   };
 
   const handleUserClick = () => {
-    navigate("/login");
+    if (user) {
+      navigate("/account"); // Redirige a la cuenta si está autenticado
+    } else {
+      navigate("/login"); // Redirige al login si no está autenticado
+    }
   };
 
   const handleLogoutClick = () => {
-    logout(); 
-    navigate("/"); 
+    logout();
+    navigate("/");
   };
-
 
   const goToHome = () => {
     navigate("/");
@@ -48,16 +51,25 @@ const Navbar = ({ onSearch }) => {
   return (
     <nav className="flex items-center justify-between p-2 bg-white relative">
       {/* Logo */}
-      <h1 className="text-3xl font-bold text-slate-500" onClick={goToHome}>3legant</h1>
+      <h1 className="text-3xl font-bold text-slate-500" onClick={goToHome}>
+        3legant
+      </h1>
 
       {/* Enlaces de navegación */}
       <ul className="flex gap-6">
-        <li className="text-black hover:text-gray-600 cursor-pointer" onClick={goToHome}>Home</li>
+        <li
+          className="text-black hover:text-gray-600 cursor-pointer"
+          onClick={goToHome}
+        >
+          Home
+        </li>
 
-       {/* Menú desplegable para Productos */}
-      <PanelCategories/>
+        {/* Menú desplegable para Productos */}
+        <PanelCategories />
 
-        <li className="text-black hover:text-gray-600 cursor-pointer">Contact Us</li>
+        <li className="text-black hover:text-gray-600 cursor-pointer">
+          Contact Us
+        </li>
       </ul>
 
       {/* Íconos del usuario */}
@@ -69,49 +81,58 @@ const Navbar = ({ onSearch }) => {
           onChange={handleSearchInput}
           className="w-96 border border-gray-300 p-2 rounded-md text-gray-700 shadow-sm focus:border-blue-400 focus:ring focus:ring-blue-100 transition duration-200"
         />
-        <li className="text-black hover:text-gray-600 cursor-pointer" onClick={handleSearchClick}>
+        <li
+          className="text-black hover:text-gray-600 cursor-pointer"
+          onClick={handleSearchClick}
+        >
           <FaSearch size={20} />
         </li>
-        {user ? (
-          <div className="relative">
-            {/* Mostrar el nombre del usuario si esta logueado */}
-            <li className="font-semibold text-black hover:text-gray-600 cursor-pointer">Hola {user.name}!</li>
-            
-          </div>
-        ) : (
-          <li className="text-black hover:text-gray-600 cursor-pointer" onClick={() => navigate("/login")}>
-            <FaUser size={20} />
-          </li>
-        )}
+
+        {/* Ícono de usuario y saludo */}
+        <div
+          className="relative flex items-center gap-2 cursor-pointer"
+          onClick={handleUserClick}
+        >
+          <FaUser size={20} className="text-black hover:text-gray-600" />
+          {user && (
+            <span className="font-semibold text-black">Hola, {user.name}!</span>
+          )}
+        </div>
+
+        {/* Ícono de wishlist */}
         <div className="relative">
-          <li className="text-black hover:text-rose-600 cursor-pointer" onClick={handleWishlistClick}>
+          <li
+            className="text-black hover:text-rose-600 cursor-pointer"
+            onClick={handleWishlistClick}
+          >
             <FaHeart size={20} />
           </li>
-          
         </div>
+
+        {/* Ícono de carrito */}
         <div className="relative">
-          <li className="text-black hover:text-gray-600 cursor-pointer" onClick={handleCartClick}>
+          <li
+            className="text-black hover:text-gray-600 cursor-pointer"
+            onClick={handleCartClick}
+          >
             <FaShoppingCart size={20} />
           </li>
         </div>
-        <div className="relative">
-          <li className="text-black hover:text-gray-600 cursor-pointer" onClick={handleLogoutClick}>
+
+        {/* Ícono de logout solo si el usuario está autenticado */}
+        {user && (
+          <div className="relative">
+            <li
+              className="text-black hover:text-gray-600 cursor-pointer"
+              onClick={handleLogoutClick}
+            >
               <FaSignOutAlt size={20} />
-          </li>
+            </li>
           </div>
-        
+        )}
       </ul>
     </nav>
   );
 };
 
 export default Navbar;
-
-
-
-
- 
-  
-
-
-  
